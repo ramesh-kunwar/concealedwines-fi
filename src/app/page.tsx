0c048a39fe217f2084b58116e425f0abe0348375
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-01-31 16:14:08 +0545

    imporoved jumbotron ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import Jumbotron from "@/components/organisms/Jumbotron";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Jumbotron />
+      <div className="md:mt-40">
+        <h2 className="text-5xl text-center font-black">
+          Ambitious Wine Importer
+        </h2>
+        <div className="flex justify-center text-center">
+          <div>
+            <h3 className="text-2xl border-b border-black inline-block text-center my-6">
+              Quality Is More Important Than Quantity
+            </h3>
+            <div>
+              <Link
+                href={"#"}
+                className="text-xl text-center inline-block bg-black text-white px-6 py-2 rounded-3xl hover:bg-white hover:text-black hover:border border-black hover:scale-[0.9] transition-all duration-100 ease-in-out "
+              >
+                Viw All Wines
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-64"></div>
     </div>
   );
 }
--- src/components/organisms/Jumbotron.tsx
+++ src/components/organisms/Jumbotron.tsx
@@ -1,11 +1,13 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
 const Jumbotron = () => {
   return (
-    <div className="container mx-auto flex items-center h-screen">
-      <div className="grid grid-cols-2 items-center gap-8">
-        <div className="z-10">
-          <h1 className="text-6xl font-black">
+    <div className="container mx-auto flex items-center h-screen overflow-hidden">
+      <div className="grid  md:grid-cols-2 items-center gap-8">
+        <div className="z-10 order-2 md:order-1">
+          <h1 className="text-4xl lg:text-6xl font-black">
             Bertrand Machard de Gramont Nuits-saint-Georges Les Terrasses des
             Vallerots 2014
           </h1>
@@ -15,18 +17,25 @@ const Jumbotron = () => {
             cranberry, raspberry and cherry notes in these wines. Crisp
             freshness makes them ideal for a variety of foods.
           </p>
-          <Button className="bg-[#09090B] text-xl px-4 py-2  h-full ">
-            Read More
+          <Button className="relative overflow-hidden bg-[#09090B] text-xl px-4 py-2 h-full text-white border border-transparent group transition-all duration-300 ease-in-out hover:border-black">
+            {/* Text that changes color */}
+            <span className="relative z-10 transition-all duration-300 ease-in-out group-hover:text-black">
+              Read More
+            </span>
+            {/* Expanding background on hover */}
+            <span className="absolute left-0 top-0 w-0 h-full bg-white transition-all duration-500 ease-in-out group-hover:w-full"></span>
           </Button>
         </div>
-        <div className="-z-10">
-          <Image
-            src="/images/wine-header.webp"
-            width={500}
-            height={500}
-            alt="Header Wine"
-            className=" rotate-[30deg] -z-10 ml-12"
-          />
+        <div className="-z-10 md:order-2">
+          <div className="rotate-[30deg]">
+            <Image
+              src="/images/wine-header.webp"
+              width={500}
+              height={500}
+              alt="Header Wine"
+              className="animate-floating rotate-[30deg] -z-10 ml-12"
+            />
+          </div>
         </div>
       </div>
     </div>
